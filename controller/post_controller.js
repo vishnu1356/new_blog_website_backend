a3b854bc9c5970d689369027131ed50d78b94eba
Vishnu <dummy.vishnu66@gmail.com>
2024-07-31 12:26:20 +0530

    fetch all posts

--- controller/post_controller.js
+++ controller/post_controller.js
@@ -17,5 +17,27 @@ exports.createPost = async (req, res) => {
         res.status(201).json({message: "Post created successfully"})
     } catch (error) {
         console.error("error caught by createPost", error)
+        res.status(500).json({message: "Internal Server Error"})
     }
 }
+
+
+exports.getAllPosts = async (req, res) => {
+
+    try {
+        const response = await Post.find()
+        console.log("response is", res)
+        if (response.length === 0) {
+            return res.status(404).json({ message: 'No posts found' });
+          }
+      
+          // Send the posts as the response
+          res.status(200).json(response);
+    } catch (error) {
+        console.error("error caught by getAllPost", error)
+        res.status(500).json({message: "Internal Server Error"})
+
+    }
+}
+
+
--- routes/api_routes.js
+++ routes/api_routes.js
@@ -4,7 +4,7 @@ const multer = require('multer')
 const path = require("path")
 const { createUser, loginUser } = require('../controller/user_controller')
 const { isLoggedIn } = require('../middlewares/auth_middleware')
-const { createPost } = require('../controller/post_controller')
+const { createPost, getAllPosts } = require('../controller/post_controller')
 
 const apiRouter = express.Router()
 
@@ -21,14 +21,7 @@ const storage = multer.diskStorage({
     }
 })
 
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, '../upload');
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, Date.now() + '-' + file.originalname);
-//     }
-// });
+
 const upload = multer({ storage:storage })
 
 
@@ -39,6 +32,7 @@ apiRouter.get("/ping", [isLoggedIn], (req, res) => {
 
 apiRouter.post("/user/signup", createUser)
 apiRouter.post("/user/signin", loginUser)
+apiRouter.get("/posts", getAllPosts)
 apiRouter.post("/post/create", upload.single("img"), createPost)
 
 
